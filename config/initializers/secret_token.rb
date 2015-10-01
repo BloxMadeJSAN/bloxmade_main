03196a664047ffaba561caeb156be42025b06520
BloxMadeJSAN <jon@bloxmade.com>
2015-10-01 15:56:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimplecodecastsSaas::Application.config.secret_key_base = '625547f448ca84dfadb6b0ad0c9cb570602283c5f4679ababccca8869a582d9c3112244205b7b0de62c116bd6cd49db852e562b87d7cc325cc1ea7e5f878dad7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimplecodecastsSaas::Application.config.session_store :cookie_store, key: '_simplecodecasts_saas_session'
