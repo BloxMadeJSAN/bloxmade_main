1f029c8e937713a488ad692b1852ba9e86c128cb
BloxMadeJSAN <jon@bloxmade.com>
2015-10-03 00:08:09 +0000

    added navbar bootstrap js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
     
     def about
     end
+    
+    def kickstarter
+    end
+    
+    def partner
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SimplecodecastsSaas::Application.routes.draw do
   get '/about' => 'pages#about'
+  get '/kickstarter' => 'pages#kickstarter'
+  get '/partner' => 'pages#partner'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
