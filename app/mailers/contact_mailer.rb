112ba2ded98379c7f2067172b60e9163ec160712
BloxMadeJSAN <jon@bloxmade.com>
2015-10-09 22:34:31 +0000

    testing new code for action mailer error take 1

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'help@bloxmade.com'
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        @business = business
-        @market = market
-        mail(from: email, subject: 'Contact Form Message')
-    end
+  default to: 'help@bloxmade.com'
+  def contact_email(name, email, business, market, body)
+    @name = name
+    @email = email
+    @business = business
+    @market = market
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
